5cc6c88faab5540d171eb8aecb72e093c7e9c5f7
oscaresteve <oscarestevea24@gmail.com>
2024-04-09 20:02:50 +0200

    Login - React Hook Form and Yup Validation

--- src/screens/LogIn.jsx
+++ src/screens/LogIn.jsx
@@ -6,16 +6,33 @@ import {
   Button,
   Alert,
   Pressable,
+  StyleSheet,
 } from "react-native";
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 import { getUser } from "../database/Database";
 
-import CustomTextInput from "../components/CustomTextInput";
-
 export default function LogIn({ navigation }) {
-  const { control, handleSubmit } = useForm();
+  const validationSchema = yup
+    .object()
+    .shape({
+      email: yup.string().required().email(),
+      password: yup.string().required(),
+    })
+    .required();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(validationSchema) });
+
   const handleLogIn = async (formData) => {
+    console.log(formData);
     const userSnap = await getUser(formData.email);
     if (userSnap != null) {
       if (userSnap.password == formData.password) {
@@ -34,16 +51,50 @@ export default function LogIn({ navigation }) {
   return (
     <SafeAreaView>
       <Text>LogIn</Text>
-      <CustomTextInput control={control} placeholder={"email"} name={"email"} />
-      <CustomTextInput
+
+      <Controller
+        name="email"
         control={control}
-        placeholder={"password"}
-        name={"password"}
+        render={({ field: { value, onChange, onBlur } }) => (
+          <TextInput
+            style={styles.input}
+            value={value}
+            onChangeText={onChange}
+            onBlur={onBlur}
+          />
+        )}
       />
+      {errors.email && <Text>{errors.email.message}</Text>}
+
+      <Controller
+        name="password"
+        control={control}
+        render={({ field: { value, onChange, onBlur } }) => (
+          <TextInput
+            style={styles.input}
+            value={value}
+            onChangeText={onChange}
+            onBlur={onBlur}
+          />
+        )}
+      />
+      {errors.password && <Text>{errors.password.message}</Text>}
+
       <Button title="Log In" onPress={handleSubmit(handleLogIn)} />
+
       <Pressable onPress={() => navigation.navigate("Register")}>
         <Text>¿You dont have an account? Click here and register!</Text>
       </Pressable>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 10,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 15,
+    borderRadius: 10,
+  },
+});
