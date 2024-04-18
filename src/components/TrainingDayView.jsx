c3d8bbde1b12f7a62424a642ffab2f6dd990e417
oscaresteve <oscarestevea24@gmail.com>
2024-04-18 11:26:03 +0200

    TrainingDayView

--- src/components/TrainingDayView.jsx
+++ src/components/TrainingDayView.jsx
@@ -1,41 +1,53 @@
 import { View, Text, Pressable } from "react-native";
 import React, { useEffect, useState } from "react";
 import { getUserTrainingDay, newUserTrainingDay } from "../database/Database";
-import { render } from "react-dom";
 
 export default function TrainingDayView({ email, userTrainingName }) {
   const [userTrainingDayData, setUserTrainingDayData] = useState();
 
+  const fetchData = async () => {
+    const userTrainingDaySnap = await getUserTrainingDay(email);
+    if (userTrainingDaySnap !== false) {
+      setUserTrainingDayData(userTrainingDaySnap);
+    } else {
+      newUserTrainingDay(email, userTrainingName);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      const userTrainingDaySnap = await getUserTrainingDay(email);
-      if (userTrainingDaySnap !== false) {
-        setUserTrainingDayData(userTrainingDaySnap);
-      } else {
-        newUserTrainingDay(email, userTrainingName);
-      }
-    };
     fetchData();
   }, []);
+
+  const handleDone = (groupIndex, exerciseIndex, setIndex) => {
+    setUserTrainingDayData((prevData) => {
+      const newUserTrainingDayData = { ...prevData };
+      prevData.groups[groupIndex].exercises[exerciseIndex].sets[
+        setIndex
+      ].details.done = true;
+
+      return newUserTrainingDayData;
+    });
+    console.log(JSON.stringify(userTrainingDayData, null, 2));
+  };
 
   if (userTrainingDayData) {
     return (
       <View key={userTrainingDayData.dayName}>
         <Text>{userTrainingDayData.dayName}</Text>
 
-        {userTrainingDayData.groups.map((group) => (
-          <View key={group.groupName}>
+        {userTrainingDayData.groups.map((group, groupIndex) => (
+          <View key={groupIndex}>
             <Text>{group.groupName}</Text>
 
-            {group.exercises.map((exercise) => (
-              <View key={exercise.exerciseName}>
+            {group.exercises.map((exercise, exerciseIndex) => (
+              <View key={exerciseIndex}>
                 <Text>{exercise.exerciseName}</Text>
 
-                {exercise.sets.map((set) => (
+                {exercise.sets.map((set, setIndex) => (
                   <View
-                    key={set.setNumber}
+                    key={setIndex}
                     className={`flex-row justify-between m-1 p-2 ${
-                      set.details.done ? "bg-green-50" : "bg-slate-50"
+                      set.details.done ? "bg-green-300" : "bg-gray-300"
                     }`}
                   >
                     <Text>
@@ -43,12 +55,14 @@ export default function TrainingDayView({ email, userTrainingName }) {
                       {set.details.weight}
                     </Text>
                     <Pressable
-                      onPress={() => {
-                        set.details.done = true;
-                        console.log(
-                          JSON.stringify(userTrainingDayData, null, 2)
-                        );
-                      }}
+                      onPress={() =>
+                        handleDone(groupIndex, exerciseIndex, setIndex)
+                      }
+                      disabled={
+                        setIndex === 0
+                          ? false
+                          : !exercise.sets[setIndex - 1].details.done
+                      }
                     >
                       <Text>Done</Text>
                     </Pressable>
--- src/components/TrainingView.jsx
+++ src/components/TrainingView.jsx
@@ -22,20 +22,20 @@ export default function Training({ email, userTrainingName }) {
       <View key={userTrainingData.trainingName}>
         <Text>{userTrainingData.trainingName}</Text>
 
-        {userTrainingData.days.map((day) => (
-          <View key={day.dayName}>
+        {userTrainingData.days.map((day, dayIndex) => (
+          <View key={dayIndex}>
             <Text>{day.dayName}</Text>
 
-            {day.groups.map((group) => (
-              <View key={group.groupName}>
+            {day.groups.map((group, groupIndex) => (
+              <View key={groupIndex}>
                 <Text>{group.groupName}</Text>
 
-                {group.exercises.map((exercise) => (
-                  <View key={exercise.exerciseName}>
+                {group.exercises.map((exercise, exerciseIndex) => (
+                  <View key={exerciseIndex}>
                     <Text>{exercise.exerciseName}</Text>
 
-                    {exercise.sets.map((set) => (
-                      <View key={set.setNumber}>
+                    {exercise.sets.map((set, setIndex) => (
+                      <View key={setIndex}>
                         <Text>
                           Set {set.setNumber}: Reps {set.details.reps}, Weight{" "}
                           {set.details.weight}
